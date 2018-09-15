312bb91a7ddf0171506a18a15d0fad4761cc926b
vagrant <vagrant@homestead>
2018-09-15 07:04:41 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'LaraBBS') - Laravel LaraBBS</title>
-
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
