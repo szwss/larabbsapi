cde1b9d300e78274907bffbabb81da365566d47a
vagrant <vagrant@homestead>
2018-09-16 11:58:44 +0000

    Sidebar active users

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         $this->laravelNotify($instance);
     }
     use HasRoles;
-
+    use Traits\ActiveUserHelper;
     /**
      * The attributes that are mass assignable.
      *
