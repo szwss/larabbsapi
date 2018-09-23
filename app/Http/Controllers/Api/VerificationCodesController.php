aa94ff067cfe54c9da6ff962d699eb83504dfd50
vagrant <vagrant@homestead>
2018-09-23 08:13:30 +0000

    user can register using phone

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -11,9 +11,7 @@ class VerificationCodesController extends Controller
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
         $phone = $request->phone;
-
-
-
+        
         if (!app()->environment('production')) {
             $code = '1234';
         } else {
