9d768bfed852a6cf4bda03eef2b501de55ece478
vagrant <vagrant@homestead>
2018-09-25 13:38:07 +0000

    links index

---
+++ app/Http/Controllers/Api/LinksController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+//use App\Http\Controllers\Controller;
+
+use App\Models\Link;
+use App\Transformers\LinkTransformer;
+
+class LinksController extends Controller
+{
+    public function index(Link $link)
+    {
+        $links = $link->getAllCached();
+
+        return $this->response->collection($links, new LinkTransformer());
+    }
+}
